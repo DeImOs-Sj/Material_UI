778816b01bd3e17d265d02c0ea991e2ddaae947f
Your Name <shlokjagtap123@gmail.com>
2024-02-29 19:39:24 +0530

    Project commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Sidebar from './Components/Sidebar'
+import Feed from './Components/Feed'
+import Rightbar from './Components/Rightbar'
+
+const App = () => {
+  return (
+    <div>
+      <Sidebar />
+      <Feed />
+      <Rightbar />
+    </div>
+  )
+}
+
+export default App
