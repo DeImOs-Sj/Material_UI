f56a75587d0e648b4899d97d3a8ba4c79f22c6ae
Your Name <shlokjagtap123@gmail.com>
2024-03-01 19:50:30 +0530

    application done

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Sidebar from './Components/Sidebar'
 import Feed from './Components/Feed'
 import Rightbar from './Components/Rightbar'
 import { Box,Stack,Container } from '@mui/material'
+import Navbar from './Components/Navbar'
 
 
 const App = () => {
   return (
     <Box>
+      <Navbar/>
       <Stack direction="row" justifyContent="space-between" gap={2}>
       <Sidebar />
       <Feed />
